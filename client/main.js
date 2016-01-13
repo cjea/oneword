ef5f0bbe5eca0aa8218aea0401cb6ff00b4b588c
CJ Apel <cjapel@CJs-MacBook-Air.local>
2016-01-13 12:38:16 -0800

    show text box after selecting wordsGoal

--- client/main.js
+++ client/main.js
@@ -1,9 +1,16 @@
 var ONE_WORD = angular.module("oneword", []);
 
 ONE_WORD.controller("userText", ["$scope", function($scope){
+
   var userTextVm = this;
-  this.text = "Hello, world!";
+  
   this.handleKeydown = function(event){
     console.log(event.keyCode);
-  }
+  };
+
+  this.setWordsGoal = function(numWords) {
+    console.log(numWords, typeof numWords);
+    return +numWords === +numWords ? userTextVm.wordsGoal = numWords : null;
+  };
+
 }]);
